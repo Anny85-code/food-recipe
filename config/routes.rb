5b338e6246b38c9027a0030ed53a29aa587b9fd9
uae <aniekaudo@yahoo.com>
2022-05-16 19:43:10 +0100

    :card_file_box: Inventiery controller
    Created invenyory controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'inventory/index'
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
