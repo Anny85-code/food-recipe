0c0d8de8eefcc54db8f1bd9604842b65ca5ef568
jihaneh <jijih@hotmail.fr>
2022-05-17 12:43:40 +0200

    Add Recipe Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_16_190255) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_103634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_16_190255) do
     t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.time "preparation_time"
+    t.time "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,4 +71,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_16_190255) do
   add_foreign_key "inventories", "users"
   add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
+  add_foreign_key "recipes", "users"
 end
---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  before(:each) do
+    @user = User.create(name: 'Emy', email: 'emy@dodso.fr', password: '11111111')
+    expect(@user).to be_valid
+    @recipe = Recipe.create(name: 'cookies', preparation_time: '2', cooking_time: '1', description: 'chocolat cookies', public: true, user: @user)
+  end
+
+  after(:each) do
+    Recipe.destroy_all
+
+    User.destroy_all
+  end
+
+  it 'should create a recipe' do
+    expect(@recipe).to be_valid
+    expect(@recipe.name).to eq 'cookies'
+    # expect(@recipe.preparation_time).to eq "02:00:00"
+    # expect(@recipe.cooking_time).to eq "01:00:00"
+    expect(@recipe.description).to eq 'chocolat cookies'
+    expect(@recipe.public).to eq true
+    expect(@recipe.user).to eq @user
+  end
+end
