43f2ed0ed78751bb09ff9799685f9f8576609b12
jihaneh <jijih@hotmail.fr>
2022-05-18 16:29:46 +0200

    Update index ans show for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,10 +3,13 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!, only: %i[new create destroy]
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user: current_user)
   end
 
-  def show; end
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'public_recipes/404' if !@recipe.public && (!user_signed_in? || @recipe.user != current_user)
+  end
 
   def new
     @recipe = Recipe.new
@@ -19,32 +22,26 @@ class RecipesController < ApplicationController
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to recipe_url(@recipe), notice: 'recipe was successfully created.' }
-        format.json { render :show, status: :created, location: @recipe }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /recipes/1 or /recipes/1.json
   def destroy
     @recipe.destroy
 
     respond_to do |format|
       format.html { redirect_to recipes_url, notice: 'recipe was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_recipe
     @recipe = Recipe.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
