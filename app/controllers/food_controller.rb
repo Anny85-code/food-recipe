74a825ff8576ca6f14bc0e85366e0c6e36c5cff3
uae <aniekaudo@yahoo.com>
2022-05-18 09:00:50 +0100

    :rotating_light: Linter
    Fixing linters

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -9,12 +9,11 @@ class FoodController < ApplicationController
 
   def destroy
     @post = Food.find(params[:id])
-    user = User.find(@food.user_id)
+    # user = User.find(@food.user_id)
     @food.destroy if @food.present?
     food.save
     respond_to do |format|
       format.html { redirect_to user_path(id: @food.user_id), notice: 'food was removed.' }
     end
   end
-
 end
