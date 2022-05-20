8aaee96426623d35e255d1263292f338e00c05e3
jihaneh <jijih@hotmail.fr>
2022-05-20 12:09:07 +0200

    Add n+1 in public recipe controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -6,7 +6,7 @@ class PublicRecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.includes(:recipe_foods).find(params[:id])
     render 'public_recipes/404' if !@recipe.public && (!user_signed_in? || @recipe.user != current_user)
   end
 end
