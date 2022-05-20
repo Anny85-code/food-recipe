b305590ec74fcb8c54bbb41bd0b77eca9061219c
jihaneh <jijih@hotmail.fr>
2022-05-20 07:07:25 +0200

    Update General Shopping

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   
   root "pages#home"
-  get 'public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+  get 'public_recipes', to: 'public_recipes#index', as: 'public_recipes'  
+  get 'shopping', to: 'shopping#index', as: 'shop'
+
+
+  get 'recipes/:id', to: 'public_recipes#show', as: 'public_recipe_show'
   resources :recipes, only: %i[index new show create destroy] 
 
-  get 'shopping', to: 'shopping#index', as: 'shop'
-  get 'recipes/:id', to: 'public_recipes#show', as: 'public_recipe_show'
   get 'inventory_food/index'
   get 'inventory/index'
 
