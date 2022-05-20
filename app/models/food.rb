5b1d5626ab34bdbe280e165ea0047463d67bcd7c
jihaneh <jijih@hotmail.fr>
2022-05-20 13:36:51 +0200

    Update ability and create Error page

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,8 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :inventory_foods, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :measuring_unit, presence: true, comparison: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true, comparison: { greater_than_or_equal_to: 0 }
 end
