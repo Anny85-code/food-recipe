6029af72c77fbc08fe29d795ac7c4bfa5a8d4080
uae <aniekaudo@yahoo.com>
2022-05-20 15:08:06 +0100

    :rotating_light: Rubocop
    Fixing linters errors

--- spec/views/recipes/show.html.erb_spec.rb
+++ spec/views/recipes/show.html.erb_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'recipes/show', type: :system do
   before(:each) do
     ally = User.create(name: 'Ally', email: 'ally@recipe.com', password: '11111111')
-    @recipe_1 = Recipe.create!(name: 'Cook1', preparation_time: '10min', cooking_time: '20min', description: 'MyText', public: true, user: ally)
+    @recipe1 = Recipe.create!(name: 'Cook1', preparation_time: '10min', cooking_time: '20min', description: 'MyText', public: true, user: ally)
   end
 
   it 'I can see the Recipe details' do
-    visit recipe_path(@recipe_1.id)
+    visit recipe_path(@recipe1.id)
     expect(page).to have_content 'Cook1'
     expect(page).to have_content '10min'
     expect(page).to have_content '20min'
